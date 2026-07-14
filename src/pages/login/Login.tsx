c45de4bd019580ee94c00ed9b257ef6cf08eeab0
Flame <parasouzaf@gmail.com>
2026-07-14 20:30:05 -0300

    Consumindo o Back e fazendo login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,46 @@
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState, type ChangeEvent, type FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+import type UsuarioLogin from "../../models/UsuarioLogin";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Login() {
+
+    const navigate = useNavigate();
+
+    const { usuario, handleLogin, isLoading } = useContext(AuthContext);
+
+    const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
+        {} as UsuarioLogin
+    );
+
+    useEffect(() => {
+        if (usuario.token !== "") {
+            navigate('/home');
+        }
+    }, [usuario]);
+
+    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+        setUsuarioLogin({
+            ...usuarioLogin,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    function login(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        handleLogin(usuarioLogin);
+    }
 
     return (
         <>
             <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
-                <form className="flex justify-center items-center flex-col w-1/2 gap-4">
+                <form className="flex justify-center items-center flex-col w-1/2 gap-4"
+                    onSubmit={login}>
+
                     <h2 className="text-slate-900 text-5xl">Entrar</h2>
-                    
+
+
                     <div className="flex flex-col w-full">
                         <label htmlFor="usuario">Usuário</label>
                         <input
@@ -16,7 +49,9 @@ function Login() {
                             name="usuario"
                             placeholder="Usuário"
                             className="border-2 border-slate-700 rounded p-2"
-                        
+                            value={usuario.nome}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+
                         />
                     </div>
 
@@ -28,7 +63,9 @@ function Login() {
                             name="senha"
                             placeholder="Senha"
                             className="border-2 border-slate-700 rounded p-2"
-                        
+                            value={usuario.nome}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+
                         />
                     </div>
 
@@ -36,7 +73,13 @@ function Login() {
                         type="submit"
                         className="rounded bg-indigo-400 flex justify-center
                                 hover:bg-indigo-900 text-white w-1/2 py-2">
-                        <span>Entrar</span>
+                        {isLoading ?
+                            <ClipLoader
+                                color="#ffffff"
+                                size={24}
+                            /> :
+                            <span>Cadastrar</span>
+                        }
                     </button>
 
                     <hr className="border-slate-800 w-full" />
