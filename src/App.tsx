d86e9297dd586a9a203b6c1d55bf85dbb62665f9
Flame <parasouzaf@gmail.com>
2026-07-15 01:02:24 -0300

    CRUD Tema Complet

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import Cadastro from "./pages/cadastro/Cadastro";
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/tema/listatemas/ListaTemas';
+import FormTema from './components/tema/formtema/FormTema';
+import DeletarTema from './components/tema/deletartema/DeletarTema';
 
 function App() {
   return (
     <>
+
       <AuthProvider>
         <BrowserRouter>
           <Navbar />
@@ -17,13 +21,15 @@ function App() {
               <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
       </AuthProvider>
-
     </>
   );
 }
